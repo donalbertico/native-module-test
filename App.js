43cd545b5edc371865261d5fe5f56b84e355e445
donalbertico <albertotates@gmail.com>
2021-10-12 11:13:06 +0100

    creds off

--- App.js
+++ App.js
@@ -11,13 +11,7 @@ import Modulin from './NativeModule'
 
 export default function App() {
  const firebaseC = {
-  apiKey: "AIzaSyDU-AvUc5z3SgMabD0dgTXIjSdjy7sTkrk",
-  authDomain: "airize-development.firebaseapp.com",
-  projectId: "airize-development",
-  storageBucket: "airize-development.appspot.com",
-  messagingSenderId: "420079497855",
-  appId: "1:420079497855:web:6ab07042945c415a14454a",
-  measurementId: "G-JRQ8TDGJ2N"
+
 }
   const { DEFAULT_EVENT_NAME } = Modulin.getConstants();
   const discovery = {
